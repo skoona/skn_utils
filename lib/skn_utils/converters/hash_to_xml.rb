3961f9aed3cf2c35649abce1ec9757418ef0a546
James Scott Jr <jscott@skoona.net>
2017-09-04 23:05:04 -0400

    Adding HashToXml class as a converter.

--- lib/skn_utils/converters/hash_to_xml.rb
+++ lib/skn_utils/converters/hash_to_xml.rb
@@ -2,8 +2,32 @@
 #
 # Converts Simple Hash to XML, or a Complex nested hash to XML
 # Ref: https://stackoverflow.com/questions/11933451/converting-nested-hash-into-xml-using-nokogiri
-# Ref: https://codereview.stackexchange.com/questions/51569/building-xml-by-enumerating-through-array-hashes
-
+#
+# Input Format:
+# {
+#   someKey: someValue,
+#   list: {item: [1,2,3]},
+#   listings: {
+#       uri: 'topic/content/topic_value',
+#       '@attributes' => {secured: true}
+#   }
+# }
+#
+# - produces:
+# <?xml version="1.0"?>
+# <root>
+#   <one>1</one>
+#   <two>2</two>
+#   <list>
+#     <item>1</item>
+#     <item>2</item>
+#     <item>3</item>
+#   </list>
+#   <listings secured="true">
+#     <uri>topic/content/topic_value</uri>
+#   </listings>
+# </basic>
+##
 
 module SknUtils
   module Converters
@@ -11,60 +35,33 @@ module SknUtils
     class HashToXml
 
       def initialize(*args)
-        @source = args.shift
       end
 
-      def self.call(*args, &block)
-        new(*args).call(&block)
+      def self.call(*array_with_at_least_one_hash, &block)
+        new().call(*array_with_at_least_one_hash, &block)
       end
 
-      def call()
-        hash = generate_xml_from_nested_hashes(@source)
+      def call(*array_with_at_least_one_hash, &block)
+        return nil if array_with_at_least_one_hash.size < 1
+        hash = generate_xml(*array_with_at_least_one_hash )
           yield hash if block_given?
         hash
-      end
-
-      def hash_simple_to_xml(hash, base_type='root', collection_key='Collection')
-        generate_xml_from_hash(hash, base_type, collection_key)
-      end
-      def hash_to_xml(data, parent = false, opt = {})
-        generate_xml_from_nested_hashes(data, parent, opt)
       end
 
     protected
 
       ##
-      # generate xml from a regular hash, with/out arrays
-      def to_xml(data)
-        Nokogiri::XML::Builder.new do |xml|
-          xml.root do                           # Wrap everything in one element.
-            handle_array('category',data,xml)  # Start the recursion with a custom name.
-          end
-        end.to_xml
-      end
-
-      ##
-      # generate xml from a regular hash, with/out arrays
-      def generate_xml_from_hash(hash, base_type, collection_key)
-        builder = ::Nokogiri::XML::Builder.new do |xml|
-          xml.send(base_type) { process_simple_array(collection_key, hash, xml) }
-        end
-
-        builder.to_xml
-      end
-
-      ##
       # generate xml from a hash of hashes or array of hashes
-      def generate_xml_from_nested_hashes(data, parent = false, opt = {})
+      def generate_xml(data, parent = false, opt = {})
         return if data.to_s.empty?
         return unless data.is_a?(Hash)
 
         unless parent
           # assume that if the hash has a single key that it should be the root
-          root, data = (data.length == 1) ? data.shift : ["root", data]
+          root, data = (data.length == 1) ? [data.keys.first.to_s, data] : ["root", data]
           builder = Nokogiri::XML::Builder.new(opt) do |xml|
             xml.send(root) {
-              generate_xml_from_nested_hashes(data, xml)
+              generate_xml(data, xml)
             }
           end
 
@@ -79,45 +76,18 @@ module SknUtils
             parent.send(label, attrs, text) {
               value.delete('@attributes')
               value.delete('@text')
-              generate_xml_from_nested_hashes(value, parent)
+              generate_xml(value, parent)
             }
 
           elsif value.is_a?(Array)
             value.each do |el|
               # lets trick the above into firing so we do not need to rewrite the checks
               el = {label => el}
-              generate_xml_from_nested_hashes(el, parent)
+              generate_xml(el, parent)
             end
 
           else
             parent.send(label, value)
-          end
-        end
-      end
-
-      private
-
-      # support for #to_xml
-      def handle_array(label,array,xml)
-        array.each do |hash|
-          xml.send(label) do                 # Create an element named for the label
-            hash.each do |key,value|
-              if value.is_a?(Array)
-                handle_array(key,value,xml) # Recurse
-              else
-                xml.send(key,value)          # Create <key>value</key> (using variables)
-              end
-            end
-          end
-        end
-      end
-
-      # support for #generate_xml_from_hash
-      def process_simple_array(label,array,xml)
-        array.each do |hash|
-          kids,attrs = hash.partition{ |k,v| v.is_a?(Array) }
-          xml.send(label,Hash[attrs]) do
-            kids.each{ |k,v| process_simple_array(k,v,xml) }
           end
         end
       end
--- spec/lib/skn_utils/converters/hash_to_xml_spec.rb
+++ spec/lib/skn_utils/converters/hash_to_xml_spec.rb
@@ -14,51 +14,102 @@ describe SknUtils::Converters::HashToXml,  "Hash to XML Converter Utility " do
       expect( described_class.call()).to be nil
     end
 
-    it "#new().hash_simple_to_xml() called with params returns xml string as expected. " do
-      obj = described_class.new()
-      string = obj.hash_simple_to_xml([{one: 1, two: 2, three: 3}])
-      puts __method__, string
-      expect(string).to be_a String
-    end
-    it "#new().hash_to_xml() called with params returns xml string as expected. " do
-      obj = described_class.new()
-      string = obj.hash_to_xml([{one: 1, two: 2, three: [1,2,3]}])
-      puts __method__, string
-      expect(string).to be_a String
-    end
-    it "#new().to_xml() called with params returns xml string as expected. " do
-      obj = described_class.new()
-      string = obj.send(:to_xml, [{one: 1, two: 2, three: [1,2,3]}])
-      puts __method__, string
-      expect(string).to be_a String
-    end
-
     it "#call called with params returns xml string as expected. " do
-      string = described_class.call([{one: 1, two: 2, three: [1,2,3]}])
+      string = described_class.call({
+                                      one: 1,
+                                      two: 2,
+                                      list: {item: [1,2,3]},
+                                      listings: {item: [1,2,3]}
+                                    })
       puts __method__, string
       expect(string).to be_a String
     end
 
     it "#call called with params and block yields xml string as expected. " do
-      described_class.call([{one: 1, two: 2, three: [1,2,3]}]) do |string|
-        expect(string).to be_a String
+      described_class.call({
+                             one: 1,
+                             two: 2,
+                             list: {item: [1,2,3]},
+                             listings: {item: [1,2,3]}
+                           }) do |string|
         puts __method__, string
+        expect(string).to be_a String
       end
     end
 
   end
 
   context "Handles Complex Hashes. " do
+    let(:array_of_hashes) {
+        {'arrayOfHashes' => [
+                      { 'name' => 'category1',
+                        'subCategory' => [
+                            { 'name' => 'subCategory1',
+                              'product' => [
+                                  { 'name' => 'productName1',
+                                    'desc' => 'desc1' },
+                                  { 'name' => 'productName2',
+                                    'desc' => 'desc2' } ]
+                            } ]
+                      },
+                      { 'name' => 'category2',
+                        'subCategory' => [
+                            { 'name' => 'subCategory2.1',
+                              'product' => [
+                                  { 'name' => 'productName2.1.1',
+                                    'desc' => 'desc1' },
+                                  { 'name' => 'productName2.1.2',
+                                    'desc' => 'desc2' } ]
+                            } ]
+                      }
+                   ]
+        }
+    }
 
+    let(:xml_aware_hash) {
+                    {'xmlAwareHash' =>
+                      {
+                        'num' => 99,
+                        'title' => 'something witty',
+                        'nested' => {
+                          'total' => [99, 98],
+                          '@attributes' => {'foo' => 'bar', 'hello' => 'world'}
+                        },
+                        'anothernest' => {
+                          '@attributes' => {'foo' => 'bar', 'hello' => 'world'},
+                          'date' => [
+                            'today',
+                            {'day' => 23, 'month' => 'Dec',
+                              'year' => {'y' => 1999, 'c' => 21},
+                              '@attributes' => {'foo' => 'blhjkldsaf'}
+                            }
+                          ]
+                        }
+                      }
+                    }
+    }
 
     it "#call(nested_hash) return an xml string as expected." do
-      string = described_class.call({one: 1, two: 2, three: [1,2,3]})
+      string = described_class.call({
+                                      one: 1,
+                                      two: 2,
+                                      list: {item: [1,2,3]},
+                                      listings: {
+                                          uri: 'topic/content/topic_value',
+                                          '@attributes' => {secured: true}}
+                                    })
       puts __method__, string
       expect( string ).to be_a String
     end
 
     it "#call(array_of_hashes) return an xml string as expected." do
-      string = described_class.call({one: 1, two: 2, three: [1,2,3]})
+      string = described_class.call(array_of_hashes, false)
+      puts __method__, string
+      expect( string ).to be_a String
+    end
+
+    it "#call(xml_aware_hash) return an xml string as expected." do
+      string = described_class.call(xml_aware_hash)
       puts __method__, string
       expect( string ).to be_a String
     end
