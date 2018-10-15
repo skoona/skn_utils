adb4207cfb86b2c0cda69c1f67cf6b6fae118048
James Scott <JScott@brotherhoodmutual.com>
2018-10-15 18:10:40 -0400

    WIP Updating COnfig.

--- lib/skn_utils/configuration.rb
+++ lib/skn_utils/configuration.rb
@@ -26,7 +26,7 @@ module SknUtils
       @default_mode = ENV.fetch('RACK_ENV', 'development')
       @config_filename = params.is_a?(String) ? params : params.fetch(:config_filename, @default_mode)
       test_mode = ENV.fetch('TEST_GEM', 'rails').eql?('gem')
-      @base_path = config_path!( test_mode ? './spec/factories' : './config' )
+      config_path!( test_mode ? './spec/factories' : './config' )
       load_config_basename!(@config_filename)
     end
 
@@ -34,15 +34,23 @@ module SknUtils
       reset_from_empty!(load_config(conf), false) # enable dot notation via defined methods(true) vs method_missing(false)
       self
     end
-    alias_method :reload!, :load_config_basename!
+
+    def reload!
+      reset_from_empty!( load_ordered_list(@_last_filelist) )
+      self
+    end
 
     def config_path!(fpath)
-      @base_path = fpath if File.exist?("#{fpath}settings.yml")
-      @base_path = "#{fpath}/" if File.exist?("#{fpath}/settings.yml")
+      if File.exist?("#{fpath}settings.yml")
+        @base_path = fpath
+      elsif File.exist?("#{fpath}/settings.yml")
+        @base_path = "#{fpath}/"
+      end
     end
 
     def load_and_set_settings(ordered_list_of_files)
       reset_from_empty!( load_ordered_list( ordered_list_of_files ) )
+      self
     end
     alias_method :reload_from_files, :load_and_set_settings
 
@@ -53,25 +61,20 @@ module SknUtils
       configuration_files()
     end
 
-
     def add_source!(file_path_or_hash) # load last
-      return {} if File.exist.eql?( file_path_or_hash )
-      ordered_list_of_files = configuration_files().push( file_path_or_hash )  # or unshift
-      load_and_set_settings(ordered_list_of_files)
-
-      # Settings.add_source!({some_secret: ENV['some_secret']})
+      return {} unless file_path_or_hash.is_a?(Hash) || File.exist?( file_path_or_hash )
+      load_and_set_settings( (configuration_files() + [file_path_or_hash]).flatten )
     end
 
     def prepend_source!(prepend_fpath) # load first
-      return {} if File.exist.eql?( prepend_fpath )
-      ordered_list_of_files = configuration_files().unshift(prepend_path)  # or unshift
-      load_and_set_settings(ordered_list_of_files)
+      return {} unless prepend_fpath.is_a?(Hash) || File.exist?( prepend_fpath )
+      load_and_set_settings( ([prepend_path] + configuration_files()).flatten )
     end
 
     private
 
     def configuration_files(default=@default_mode)
-      @_last_filelist = [
+      [
           "#{@base_path}settings.yml",
           "#{@base_path}settings/#{default}.yml",
           "#{@base_path}environments/#{default}.yml",
@@ -82,34 +85,32 @@ module SknUtils
     end
 
     def load_ordered_list(filelist)
-      yname = filelist.shift
-        return {} unless File.exist?( yname )
-        f_base  = load_yml_with_erb(yname)
+      settings_file = nil
+      yname = filelist.first
+        return {} unless  yname.is_a?(Hash) || (yname && File.exist?( yname ))
 
+      # maintain last list for :reload
+      @_last_filelist = filelist
+
+      settings_file = yname if yname.include?('settings.yml')
+
+      f_base  = {}
       filelist.each do |filepath|
-        next unless File.exist?(filepath)
-        f_env   = load_yml_with_erb(filepath)
+        next unless filepath.is_a?(Hash) || File.exist?(filepath)
+        settings_file = filepath if filepath.include?('settings.yml')
+        f_env   = filepath.is_a?(Hash) ? filepath : load_yml_with_erb(filepath)
         f_base  = f_base.deep_merge!(f_env) unless (f_env.nil? || f_env.empty?)
+      end
+
+      unless settings_file.nil?
+        config_path!( File.dirname(settings_file) ) # maintain root dir
       end
 
       f_base
     end
 
     def load_config(conf)
-      yname   = "#{@base_path}settings.yml"
-        return {} unless File.exist?(yname)
-        f_base  = load_yml_with_erb(yname)
-
-     do_this = [
-         "#{@base_path}settings.yml",
-         "#{@base_path}settings/#{conf}.yml",
-         "#{@base_path}settings/environments/#{conf}.yml",
-         "#{@base_path}settings.local.yml",
-         "#{@base_path}settings/#{conf}.local.yml",
-         "#{@base_path}settings/environments/#{conf}.local.yml"
-     ]
-
-      load_ordered_list( do_this )
+      load_ordered_list( configuration_files(conf) )
     end
 
     def load_yml_with_erb(yml_file)
--- spec/lib/skn_settings_spec.rb
+++ spec/lib/skn_settings_spec.rb
@@ -2,21 +2,63 @@
 
 describe SknSettings, "Application Configuration" do
 
+  let(:short_list) do
+    ["./spec/factories/settings/test.yml", "./spec/factories/settings/test.local.yml", "./spec/factories/settings.yml"]
+  end
+
   it 'contains the test settings for the application' do
-    expect(SknSettings.Packaging.pomVersion).to eq SknUtils::VERSION
-    expect(SknSettings.Packaging.configName).to eq 'test.local'
-    expect(SknSettings.Packaging.isTest).to be true
+    expect(described_class.Packaging.pomVersion).to eq SknUtils::VERSION
+    expect(described_class.Packaging.configName).to eq 'development'
+    expect(described_class.Packaging.isTest).to be true
   end
 
   it 'contains the current RACK or RAILS environment values' do
-    expect(SknSettings.env).to eq 'test'
-    expect(SknSettings.env.test?).to be true
-    expect(SknSettings.env.development?).to be false
+    expect(described_class.env).to eq 'test'
+    expect(described_class.env.test?).to be true
+    expect(described_class.env.development?).to be false
   end
 
   it 'contains the application root path as a string' do
-    expect(SknSettings.root).to eq Dir.pwd
-    expect(SknSettings.root).to be_a(String)
+    expect(described_class.root).to eq Dir.pwd
+    expect(described_class.root).to be_a(String)
   end
+
+
+  it "#load_config_basename!(conf=@default_mode) load correct environments. " do
+    %w(development).each do |env_mode|
+      expect(described_class.load_config_basename!(env_mode).Packaging.configName).to eq env_mode
+    end
+  end
+
+  it "#reload! reloads the current list of files. " do
+    expect( described_class.reload!().env.test? ).to be true
+    expect( described_class.reload!().env.test? ).to be true
+  end
+
+  it "#config_path!(fpath) set the configfuration root path properly. " do
+    described_class.config_path!('./spec/factories/')
+    expect( described_class.instance_variable_get(:@base_path) ).to eq('./spec/factories/')
+
+    subject.config_path!('./spec/factories')
+    expect( described_class.instance_variable_get(:@base_path) ).to eq('./spec/factories/')
+  end
+
+  it "#load_and_set_settings(ordered_list_of_files) " do
+    expect( described_class.load_and_set_settings(short_list).env.test? ).to be true
+  end
+
+  # Config.setting_files("/path/to/config_root", "your_project_environment")
+  it "#setting_files(config_root, env_name) returns a file array. " do
+    expect(described_class.settings_files('spec/factories', 'test')).to be_a(Array)
+  end
+
+  it "#add_source!(file_path_or_hash) adds provided file to end of list. " do
+    expect(described_class.add_source!({Packaging: {configName: 'The Doctor'}}).Packaging.configName ).to eq('The Doctor')
+  end
+
+  it "#prepend_source!(prepend_fpath) adds provided value to front of list. " do
+    expect(described_class.prepend_source!({Packaging: {configName: 'The Doctor'}}).Packaging.configName ).to eq('test.local')
+  end
+
 
 end
