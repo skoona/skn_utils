83a01d3172d6543619f997db961c191533a010ac
James Scott Jr <skoona@gmail.com>
2018-12-31 10:10:39 -0500

    Add Wrapable Module utils as an RSpec test for evaluation.

---
+++ spec/lib/skn_utils/wrappers_spec.rb
@@ -0,0 +1,107 @@
+# ##
+#
+# Ref: https://blog.appsignal.com/2018/10/02/ruby-magic-class-level-instance-variables.html
+
+
+module Wrappable
+  def wrap(mod)
+    wrappers << mod
+  end
+
+  def wrappers
+    @wrappers ||= []
+  end
+
+  def inherited_wrappers
+    ancestors
+        .grep(Wrappable)
+        .reverse
+        .flat_map(&:wrappers)
+  end
+
+  def new(*arguments, &block)
+    instance = allocate
+    inherited_wrappers.each { |mod|instance.singleton_class.include(mod) }
+    instance.send(:initialize, *arguments, &block)
+    instance
+  end
+end
+
+
+module Powered
+  def make_noise
+    puts "Powering up"
+    super
+    puts "Shutting down"
+  end
+end
+
+class Machine
+  extend Wrappable
+
+  wrap Powered
+
+  def make_noise
+    puts "Buzzzzzz"
+  end
+end
+
+
+module Logging
+  def make_noise
+    puts "Started making noise"
+    super
+    puts "Finished making noise"
+  end
+end
+
+class Bird
+  extend Wrappable
+
+  wrap Logging
+
+  def make_noise
+    puts "Chirp, chirp!"
+  end
+end
+
+
+module Flying
+  def make_noise
+    super
+    puts "Is flying away"
+  end
+end
+
+class Pigeon < Bird
+  wrap Flying
+
+  def make_noise
+    puts "Coo!"
+  end
+end
+
+describe 'Wrappable Module Handles Inheritance properly ' do
+
+  it '#make_noise handles Bird module. ' do
+    expect do
+      obj = Bird.new
+      obj.make_noise
+    end.to output("Started making noise\nChirp, chirp!\nFinished making noise\n").to_stdout
+  end
+
+  it '#make_noise handles Pigeon module. ' do
+    expect do
+      obj = Pigeon.new
+      obj.make_noise
+    end.to output("Started making noise\nCoo!\nFinished making noise\nIs flying away\n").to_stdout
+  end
+
+  it '#make_noise handles Machine module. ' do
+    expect do
+      obj = Machine.new
+      obj.make_noise
+    end.to output("Powering up\nBuzzzzzz\nShutting down\n").to_stdout
+  end
+
+end
